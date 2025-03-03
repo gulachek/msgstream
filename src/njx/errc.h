4f54d9633f96e589a4a0fd0807f0f701f1f0659e
Nicholas Gulachek <nsgulachek@gmail.com>
2025-03-02 22:46:20 -0600

    Add copyright headers to files

--- src/njx/errc.h
+++ src/njx/errc.h
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2025 Nicholas Gulachek
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
 #ifndef MSGSTREAM_ERRC_H
 #define MSGSTREAM_ERRC_H
 
--- test/msgstream_test.cpp
+++ test/msgstream_test.cpp
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2025 Nicholas Gulachek
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
 #define BOOST_TEST_MODULE example
 #include <boost/test/unit_test.hpp>
 
