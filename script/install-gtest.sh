03c363d4721dbc34a1c89291cb5e548570343c59
Nicholas Gulachek <nsgulachek@gmail.com>
2025-03-09 21:19:37 -0500

    Verify sha256 of gtest in install script

--- script/install-gtest.sh
+++ script/install-gtest.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -x
+set -e
 
 if [ ! -f script/install-gtest.sh ]; then
 	echo "Please run from project root!"
@@ -9,13 +10,26 @@ fi
 
 . script/util.sh
 
-ARCHIVE="https://github.com/google/googletest/archive/refs/heads/main.tar.gz"
+download \
+	-u "https://github.com/google/googletest/archive/refs/heads/main.tar.gz" \
+	-c "2be01a3a16c58a72edd48cd11f0457da8cc9bcfbface2425011a444928ba9723" \
+	-o "$VENDORSRC/gtest-download.tgz"
+	
 GTEST="$VENDORSRC/googletest"
 
 md "$GTEST"
 
-untar -d "$GTEST" -u "$ARCHIVE"
+# TODO - seems like I'm recreating package managers.
+# I like being explicit about all dependencies being pulled
+# in for licensing concerns, etc. However, this seems like
+# potentially a solved problem that I'm rescripting. Albeit,
+# this isn't that much work to maintain right now.
+
+untar -f "$VENDORSRC/gtest-download.tgz" -d "$GTEST" 
 
 cmake -DBUILD_GMOCK=OFF "-DCMAKE_INSTALL_PREFIX=$VENDOR" -S "$GTEST" -B "$GTEST/build"
 cmake --build "$GTEST/build"
-cmake --install "$GTEST/build" --prefix "$VENDOR"
+cmake --install "$GTEST/build"
+
+# Test to see if conan exists in GitHub runner
+which conan
--- script/util.sh
+++ script/util.sh
@@ -6,16 +6,65 @@ md() {
 	fi
 }
 
-untar() {
-	local dir
-	local file
-	local url
+sha256() {
+	local infile="${1:?Usage: sha256 <file>}"
 
-	while getopts "u:f:d:" opt; do
+	# SHA2-256(<file>)= <hash>
+	local pieces=($(openssl dgst -sha256 "$infile"))
+	local npieces=${#pieces[*]}
+
+	# Last piece in case <file> had spaces
+	echo ${pieces[$npieces - 1]}
+}
+
+# Usage: download -c <sha256> -u <url> -o <output>
+download() {
+	local outfile
+	local expect_hash
+	local url
+	local usage="Usage: download -c <sha256> -u <url> -o <outfile>"
+
+	OPTIND=1
+	while getopts "c:u:o:" opt; do
 		case "$opt" in
+			c)
+				expect_hash="$OPTARG"
+				;;
 			u)
 				url="$OPTARG"
 				;;
+			o)
+				outfile="$OPTARG"
+				;;
+			?)
+				echo "$usage"
+				return 1
+				;;
+		esac
+	done
+
+	if [ -z "$url" -o -z "$outfile" -o -z "$expect_hash" ]; then
+		echo "$usage"
+		return 1
+	fi
+
+	curl -L -o "$outfile" "$url" || return 1
+	local measured_hash="$(sha256 $outfile)"
+	if [ "$measured_hash" != "$expect_hash" ]; then
+		echo "Downloaded file '$outfile' has SHA256 of '$measured_hash', but it was expected to have SHA256 of '$expect_hash'"
+		return 1
+	fi
+
+	return 0
+}
+
+untar() {
+	local dir
+	local file
+
+	OPTIND=1
+	while getopts "d:f:" opt; do
+		case "$opt" in
 			f)
 				file="$OPTARG"
 				;;
@@ -34,12 +83,10 @@ untar() {
 
 	local ec=0
 
-	if [ -n "$url" ]; then
-		curl -L "$url" | tar xz --strip-components=1
-	elif [ -n "$file" ]; then
+	if [ -n "$file" ]; then
 		tar xfz "$file" --strip-components=1
 	else
-		echo "untar: Neither file (-f) nor url (-u) specified"
+		echo "untar: -f not specified"
 		ec=1
 	fi
 
